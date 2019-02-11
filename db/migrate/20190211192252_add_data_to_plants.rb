3f1b661ac3697622ab6587f8a200af8da047a2d6
Milena Walter <milena.marie92@gmail.com>
2019-02-11 20:27:15 +0100

    neue plant spalten

---
+++ db/migrate/20190211192252_add_data_to_plants.rb
@@ -0,0 +1,11 @@
+class AddDataToPlants < ActiveRecord::Migration[5.2]
+  def change
+    add_column :plants, :location_requirement, :string
+    add_column :plants, :fertilizer_requirement, :string
+    add_column :plants, :root_type, :string
+    add_column :plants, :water_requirements, :string
+    add_column :plants, :seed_outdoors, :boolean
+    add_column :plants, :dist_to_neighbor, :int
+    add_column :plants, :crop_time, :int
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_102945) do
+ActiveRecord::Schema.define(version: 2019_02_11_192252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_01_02_102945) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location_requirement"
+    t.string "fertilizer_requirement"
+    t.string "root_type"
+    t.string "water_requirements"
+    t.boolean "seed_outdoors"
+    t.integer "dist_to_neighbor"
+    t.integer "crop_time"
   end
 
 end
