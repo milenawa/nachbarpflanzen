0374e72960af80b1fc56dd2dd0256b9c8b6420aa
Milena Walter <milena.marie92@gmail.com>
2019-02-19 22:23:00 +0100

    Nur verfügbare Pflanzen bei neuem Nachbarn anzeigen

--- app/helpers/impacts_helper.rb
+++ app/helpers/impacts_helper.rb
@@ -1,2 +1,6 @@
 module ImpactsHelper
+    def get_no_neighbors(plant)
+        existing_ids = plant.impacts.map(&:influencer_id)
+        Plant.where.not(id: existing_ids).order(name: :asc)
+    end
 end
