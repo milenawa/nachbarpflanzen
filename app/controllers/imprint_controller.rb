bce828f9cfdea1ab3dad2e58a8bbbc080cc69fe4
Milena Walter <milena.marie92@gmail.com>
2019-02-21 23:15:37 +0100

    impressum hinzugefügt

---
+++ app/controllers/imprint_controller.rb
@@ -0,0 +1,7 @@
+class ImprintController < ApplicationController
+    
+    # GET /imprint
+    def show
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :plants
   resources :impacts
   get '/howto', to: 'how_to#show'
+  get '/imprint', to: 'imprint#show'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
